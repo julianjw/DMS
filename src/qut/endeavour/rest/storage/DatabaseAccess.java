4cab0dd91d082e0b12786bf906b8a804862113f4
Steven Turner <shkibr@hotmail.com>
2013-09-12 22:01:12 +1000

    hooked up database connection

--- src/qut/endeavour/rest/storage/DatabaseAccess.java
+++ src/qut/endeavour/rest/storage/DatabaseAccess.java
@@ -1,17 +1,56 @@
 package qut.endeavour.rest.storage;
 
-public class DatabaseAccess {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+
+
+public class DatabaseAccess {
+	
+	private final static String dbUrl = "jdbc:mysql://localhost:3306/testdb";
+	private final static String username = "root";
+	private final static String password = "";
+	
+	private static Connection  con = null;
+	
+	private static void makeConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		
+			if ( con == null ) {
+				System.out.println("DatabaseAccess: Making a new connection to the database.");
+				con = DriverManager.getConnection(dbUrl, username, password );
+			}
+//			
+//			PreparedStatement st = con.prepareStatement("select * from names");
+//			ResultSet result = st.executeQuery();
+//			
+//			while(result.next()) {
+//				System.out.println(result.getString(1) + " " + result.getString(2));
+//			}
+		} catch ( SQLException e ) {
+			System.out.println("Database error: " + e.toString());
+		} catch (ClassNotFoundException e) {
+			System.out.println("Database error: " + e.toString());
+		}
+		
+	}
+	
 	
 	
 	public static boolean validateUser(String token, String user_id) {
+		makeConnection();
 		System.out.println("DatabaseAccess: Validating session token.");
 		return true;
 	}
 
 	public static boolean validateLogin(String userId, String password) {
+		makeConnection();
 		System.out.println("DatabaseAccess: Validating login.");
-		return false;
+		return true;
 	}
 
 }
