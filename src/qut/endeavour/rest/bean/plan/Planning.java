18e65f3c625b473f01fce0b94fc63ccf6fd1c963
Steven Turner <shkibr@hotmail.com>
2013-09-14 22:40:00 +1000

    Planning beans completed

--- src/qut/endeavour/rest/bean/plan/Planning.java
+++ src/qut/endeavour/rest/bean/plan/Planning.java
@@ -1,5 +1,40 @@
 package qut.endeavour.rest.bean.plan;
 
+import javax.xml.bind.annotation.XmlRootElement;
+
+import qut.endeavour.rest.bean.plan.planning.GoalPlan;
+import qut.endeavour.rest.bean.plan.planning.HolidayPlan;
+
+@XmlRootElement
 public class Planning {
 
+	private HolidayPlan holidayPlan;
+	private GoalPlan goalPlan;
+	
+	public Planning(
+			HolidayPlan holidayPlan,
+			GoalPlan goalPlan
+			){
+		this.holidayPlan = holidayPlan;
+		this.goalPlan = goalPlan;
+	}
+	
+	public Planning() {}
+	
+	
+	public HolidayPlan getHolidayPlan() {
+		return this.holidayPlan;
+	}
+	public void setHolidayPlan( HolidayPlan holidayPlan) {
+		this.holidayPlan = holidayPlan;
+	}
+	
+	
+	public GoalPlan getGoalPlan() {
+		return this.goalPlan;
+	}
+	public void setGoalPlan( GoalPlan goalPlan) {
+		this.goalPlan = goalPlan;
+	}
+	
 }
