37f851f039a4da74887bd95ef2976d12f1a023fc
Steven Turner <shkibr@hotmail.com>
2013-09-16 18:56:31 +1000

    AuthRole 1

---
+++ src/qut/endeavour/rest/bean/AuthRole.java
@@ -0,0 +1,33 @@
+package qut.endeavour.rest.bean;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class AuthRole {
+	public static enum Role {
+		INVALID(""), CLIENT("client"), STAFF("staff");
+		
+		private final String value;
+		private Role( String value ) {
+			this.value = value;
+		}
+		
+		public String getValue() {
+			return this.value;
+		}
+	}
+	
+	Role r;
+	
+	public AuthRole(){}
+	
+	public AuthRole( Role r ) {
+		this.r = r;
+	}
+	
+	@XmlElement(name="role")
+	public String getRole() {
+		return this.r.getValue();
+	}
+}
---
+++ src/qut/endeavour/rest/resources/RoleResource.java
@@ -0,0 +1,46 @@
+package qut.endeavour.rest.resources;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.UriInfo;
+
+import qut.endeavour.rest.bean.AuthRole;
+import qut.endeavour.rest.exception.DMSClientErrorException;
+import qut.endeavour.rest.factory.AuthFactory;
+
+
+@Path("/role")
+public class RoleResource {
+	
+	private final String USER_ID_FIELD = "user_id";
+	private final String AUTH_TOKEN_FIELD = "auth_token";
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public AuthRole getJSONRole(@Context UriInfo uriInfo ) {
+		
+		MultivaluedMap<String, String> params = uriInfo.getQueryParameters();
+		
+		String userId = params.getFirst(USER_ID_FIELD);
+		String token = params.getFirst(AUTH_TOKEN_FIELD);
+		
+		// sanity checks
+		if( userId == null ) throw new DMSClientErrorException("No user_id supplied");
+		if( userId.length() < 1 ) throw new DMSClientErrorException("No user_id supplied");
+		if( token == null ) throw new DMSClientErrorException("No token supplied");
+		if( token.length() < 1 ) throw new DMSClientErrorException("No token supplied");
+		
+		
+		System.out.println("Getting user's role.");
+		System.out.println("user_id: " + userId );
+		System.out.println("Token: "+token);
+		
+		return AuthFactory.createRole(userId, token);
+		
+	}
+	
+}
