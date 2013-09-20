533735975cd52ff38061443e445a6874f94a99fa
Steven Turner <shkibr@hotmail.com>
2013-09-20 15:50:14 +1000

    can create user

--- src/qut/endeavour/rest/resources/UsersResource.java
+++ src/qut/endeavour/rest/resources/UsersResource.java
@@ -1,29 +1,16 @@
 package qut.endeavour.rest.resources;
 
-import java.io.IOException;
-
-import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.MultivaluedMap;
-import javax.ws.rs.core.UriInfo;
-import javax.xml.bind.JAXBElement;
 
-import qut.endeavour.rest.bean.AuthResponse;
-import qut.endeavour.rest.bean.AuthToken;
 import qut.endeavour.rest.bean.Verification;
 import qut.endeavour.rest.bean.admin.DMSUser;
 import qut.endeavour.rest.exception.DMSClientErrorException;
-import qut.endeavour.rest.factory.AuthFactory;
-import qut.endeavour.rest.storage.DatabaseAccess;
 import qut.endeavour.rest.utility.Users;
 
 /**
@@ -51,41 +38,33 @@ public class UsersResource {
 	
 	
 	@POST
-	@Path("/planning/{user_id: [a-zA-Z_0-9]+}/{token: [a-zA-Z_0-9]+}/{clientid: [a-zA-Z_0-9]+}")
+	@Path("/{user_id: [a-zA-Z_0-9]+}/{token: [a-zA-Z_0-9]+}")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public void newUser(
+	public Verification newUser(
 			final DMSUser newUser,
 			@PathParam("user_id") String currentUser_id,
 			@PathParam("token") String token
 			) {
 		
-		System.out.println("Existing user id: "+currentUser_id);
-		System.out.println("Existing user auth: "+token);
-		
-		System.out.println("new user_id:"+newUser.getUser_id());
-		System.out.println("new user name:"+newUser.getrName());
-		System.out.println("new user password:"+newUser.getPassword());
-		System.out.println("new user role:"+newUser.getRole());
-		
-		if ( Users.putUserInDatabase( currentUser_id, token, newUser ) ) {
-		
-		}
-//	public void newUser( final String text) {
-//		DMSUser newUser = newUserJAX.getValue();
-		//System.out.println(text);
-//		MultivaluedMap<String, String> params = uriInfo.getQueryParameters();
+//		System.out.println("Existing user id: "+currentUser_id);
+//		System.out.println("Existing user auth: "+token);
 //		
-//		String token = params.getFirst(AUTH_TOKEN_FIELD);
-//		String user_id = params.getFirst(USER_ID_FIELD);
+//		System.out.println("new user_id:"+newUser.getUser_id());
+//		System.out.println("new user name:"+newUser.getrName());
+//		System.out.println("new user password:"+newUser.getPassword());
+//		System.out.println("new user role:"+newUser.getRole());
 		
 		// sanity checks
-//		if( user_id == null ) throw new DMSClientErrorException("No user_id supplied");
-//		if( user_id.length() < 1 ) throw new DMSClientErrorException("No user_id supplied");
-//		if( token == null ) throw new DMSClientErrorException("No token supplied");
-//		if( token.length() < 1 ) throw new DMSClientErrorException("No token supplied");
+		if( currentUser_id == null ) throw new DMSClientErrorException("No user_id supplied");
+		if( currentUser_id.length() < 1 ) throw new DMSClientErrorException("No user_id supplied");
+		if( token == null ) throw new DMSClientErrorException("No token supplied");
+		if( token.length() < 1 ) throw new DMSClientErrorException("No token supplied");
 		
-		
+		if ( Users.putUserInDatabase( currentUser_id, token, newUser ) ) {
+			return new Verification(Verification.Verified.SUCCESS);
+		}
+		return new Verification(Verification.Verified.FAILURE);
 	}
 	
 	
--- src/qut/endeavour/rest/utility/Users.java
+++ src/qut/endeavour/rest/utility/Users.java
@@ -1,18 +1,14 @@
 package qut.endeavour.rest.utility;
 
 import qut.endeavour.rest.bean.admin.DMSUser;
+import qut.endeavour.rest.storage.DatabaseAccess;
 
 public class Users {
-	
 	public static boolean putUserInDatabase( String currentUser_id, String token, DMSUser user ){
-		
 		String personName = user.getrName();
 		String newUser_id = user.getUser_id();
 		String password = user.getPassword();
 		String role_name = user.getRole();
-		
-		
-		return false;
+		return DatabaseAccess.createUser(currentUser_id, token, personName, newUser_id, password, role_name);
 	}
-	
 }
