c7cf9270fc997c162420b8a7255ab11338c9ee46
Steven Turner <shkibr@hotmail.com>
2013-10-12 19:29:29 +1000

    SqlWriteJob - major overhaul
    SqlWriteJob should now be able to write to "sub-tables"

--- src/qut/endeavour/rest/utility/SqlWriteJob.java
+++ src/qut/endeavour/rest/utility/SqlWriteJob.java
@@ -5,6 +5,7 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -18,10 +19,13 @@ public class SqlWriteJob {
 	private final static char INTEGER = 'i';
 	private final static char STRING = 's';
 	private final static char DATE = 'd';
+	private final static char AUTO_INCREMENT = 'a';
 	
 	private String tableName;
 	private Map<String,Object> key;
-	private List<Map<String,Object>> table;
+	private List<Map<String,Object>> table; /* TABLE INCLUDES ALL FIELDS INCLUDING BOTH KEYS */
+	
+	private Map<String,Object> secondKey;
 	
 	
 	/**
@@ -35,91 +39,117 @@ public class SqlWriteJob {
 	 */
 	public SqlWriteJob(String tableName, Map<String,Object> key,
 			List<Map<String, Object>> table) throws DMSException {
+		this( tableName, key, null, table );
+	}
+	
+	
+	public SqlWriteJob(
+				String tableName,
+				Map<String,Object> key,
+				Map<String,Object> secondKey,
+				List<Map<String, Object>> table
+				) throws DMSException {
 		
-		if ( tableName == null )  throw new DMSException();
-		if ( key == null)  throw new DMSException();
-		if ( key.size() != 1 ) throw new DMSException();
-		if ( table == null )  throw new DMSException();
-		if ( table.size() < 1 )  throw new DMSException();
-		if ( table.get(0) == null )  throw new DMSException();
-		if ( table.get(0).size() < 1 )  throw new DMSException();
+		String errorMessage = "Error with SqlWriteJob variables.";
+		
+		if ( tableName == null )  throw new DMSException(errorMessage);
+		if ( key == null)  throw new DMSException(errorMessage);
+		if ( key.size() != 1 ) throw new DMSException(errorMessage);
+		if ( table == null )  throw new DMSException(errorMessage);
+		if ( table.size() < 1 )  throw new DMSException(errorMessage);
+		if ( table.get(0) == null )  throw new DMSException(errorMessage);
+		if ( table.get(0).size() < 1 )  throw new DMSException(errorMessage);
 		
 		this.tableName = tableName;
 		this.key = key;
+		this.secondKey = secondKey;
 		this.table = table;
 		
 	}
 	
 	
 	public void execute() throws SQLException, DMSException {
-		
-		String keyName;
-		
-		{	
-			Entry<String, Object> entry = key.entrySet().iterator().next();
-			keyName = entry.getKey();
-		}
-		
-		if ( table.size() == 1) { // BEGIN ONE ROW ONLY
-			Map<String, Object> row = table.get(0);
-			
-			String insertSqlFields = "";//"`"+keyName.substring(2)+"`";
-			String insertSqlValues = "";//"?";
-			String updateSqlSet = "";//"`"+keyName.substring(2)+"`=?";
-			
-			for ( Entry<String, Object> field : row.entrySet() ) {
-				insertSqlFields += ",`"+field.getKey().substring(2)+"`";
-				insertSqlValues += ",?";
-				
-				if ( field.getKey() != keyName )
-					updateSqlSet += ",`"+field.getKey().substring(2)+"`=?";
-			}
-			
-			insertSqlFields = insertSqlFields.substring(1);
-			insertSqlValues = insertSqlValues.substring(1);
-			updateSqlSet = updateSqlSet.substring(1);
-			
-			String countSql = "SELECT count(tb."+keyName.substring(2)+") as `count` FROM `"+tableName+"` tb WHERE tb."+keyName.substring(2)+" = ?";
-			String insertSql = "INSERT INTO `"+tableName+"`("+insertSqlFields+") VALUES ("+insertSqlValues+")";
-			String updateSql = "UPDATE `"+tableName+"` SET "+updateSqlSet+" WHERE user_id = ?";
-	
-			System.out.println(countSql);
-			System.out.println(insertSql);
-			System.out.println(updateSql);
-			
-			System.out.println("Writing:");
-			//for ( Map.Entry<String, Object> set : row.entrySet() ) {
-			//	System.out.println("--> \""+set.getKey()+"\" - \""+set.getValue().toString()+"\"");
-			//}
-			
-			PreparedStatement countPs = populateValues(key, null, DatabaseAccess.createPreparedStatement(countSql) );
-			PreparedStatement insertPs = populateValues(table.get(0), null, DatabaseAccess.createPreparedStatement(insertSql));
-			PreparedStatement updatePs = populateValues(table.get(0), keyName, DatabaseAccess.createPreparedStatement(updateSql));
-			
-			ResultSet countRs = countPs.executeQuery(); // count how many times it is in the database
-			
-			System.out.println("");
-			
-			if( countRs.next() ) {
-				System.out.println( "number of times user id number found in table: " + countRs.getInt(1));
-				if ( countRs.getInt(1) == 0 ) { // if the count is 0, we need to insert it.
-					System.out.println("Executing an insert.");
-					insertPs.executeUpdate();
-				} else {
-					System.out.println("Executin an update.");
-					updatePs.executeUpdate();
-				}
-			}
-			
-			
-			System.out.println("Finished writing.");
-			
-		}  else { // END ONE ROW ONLY, begin multiple rows.
-			System.out.println("Multiple rows to be written to \""+ tableName +"\" - Ignoring.");
-		}
-		
+		if ( this.secondKey == null ) multiLineWrite();
+		else singleLineWrite( table.get(0) );
 	}
 
+	private void multiLineWrite() throws SQLException, DMSException {
+		for ( Map<String,Object> row: table ) singleLineWrite(row);
+	}
+	
+	private void singleLineWrite( Map<String,Object> row ) throws SQLException, DMSException {
+		if ( row == null ) throw new DMSException("Can't write null row.");
+		
+		Map<String,Object> actualKey = new HashMap<String,Object>();
+		String actualKeyName = null;
+		Object actualKeyValue = null;
+		
+		// pick the correct key
+		if ( secondKey != null ) {
+			actualKeyName = secondKey.entrySet().iterator().next().getKey();
+			actualKeyValue = secondKey.entrySet().iterator().next().getValue();
+			
+		} else {
+			actualKeyName = key.entrySet().iterator().next().getKey();
+			actualKeyValue = key.entrySet().iterator().next().getValue();
+		}
+		
+		actualKey.put( actualKeyName, actualKeyValue );
+		
+		// initialise strings
+		String insertSqlFields = "";
+		String insertSqlValues = "";
+		String updateSqlSet = "";
+		
+		// write query string datas
+		for ( Entry<String, Object> field : row.entrySet() ) {
+			insertSqlFields += ",`"+field.getKey().substring(2)+"`"; // field name
+			insertSqlValues += ",?"; // value
+			
+			// don't write key field, we'll do it manually to put it in with the where clause.
+			if ( field.getKey() != actualKeyName )
+				updateSqlSet += ",`"+field.getKey().substring(2)+"`=?";
+		}
+		
+		insertSqlFields = insertSqlFields.substring(1);
+		insertSqlValues = insertSqlValues.substring(1);
+		updateSqlSet = updateSqlSet.substring(1);
+		
+		String countSql = null;
+		String insertSql = null;
+		String updateSql = null;
+		
+		
+		countSql = "SELECT count(tb."+actualKeyName.substring(2)+") as `count` FROM `"+tableName+"` tb WHERE tb."+actualKeyName.substring(2)+" = ?;";
+		insertSql = "INSERT INTO `"+tableName+"`("+insertSqlFields+") VALUES ("+insertSqlValues+");";
+		updateSql = "UPDATE `"+tableName+"` SET "+updateSqlSet+" WHERE "+actualKeyName.substring(2)+" = ?;";
+		
+		System.out.println(countSql);
+		System.out.println(insertSql);
+		System.out.println(updateSql);
+		
+		System.out.println("SqlWriteJob: Preparing.");
+		
+		PreparedStatement countPs = populateValues(actualKey, null, DatabaseAccess.createPreparedStatement(countSql) );
+		ResultSet countRs = countPs.executeQuery(); // count how many times it is in the database
+		
+		if( countRs.next() ) {
+			System.out.println( "number of times user id number found in table: " + countRs.getInt(1));
+			if ( countRs.getInt(1) == 0 ) { // if the count is 0, we need to insert it.
+				System.out.println("Executing an insert.");
+				PreparedStatement insertPs = populateValues(row, null, DatabaseAccess.createPreparedStatement(insertSql));
+				insertPs.executeUpdate();
+				
+			} else {
+				System.out.println("Executin an update.");
+				PreparedStatement updatePs = populateValues(row, actualKeyName, DatabaseAccess.createPreparedStatement(updateSql));
+				updatePs.executeUpdate();
+			}
+		}
+		
+		System.out.println("Finished writing row with unique id - \"" + actualKeyValue.toString() +"\"");
+	}
+	
 
 	/**
 	 * Will plug values into supplied prepared statement.
@@ -128,13 +158,14 @@ public class SqlWriteJob {
 	 * @param ps
 	 * @return
 	 * @throws SQLException
+	 * @throws DMSException 
 	 */
-	private PreparedStatement populateValues(Map<String, Object> row,
+	private static PreparedStatement populateValues(Map<String, Object> row,
 			String keyName,
 			PreparedStatement ps
-			) throws SQLException {
+			) throws SQLException, DMSException {
 		
-		boolean endKey = keyName != null; // do we want the key last?
+		boolean writeKeyInWhereClause = keyName != null; // do we want the key last?
 		int total = row.size();
 		
 		int i = 0;
@@ -146,7 +177,7 @@ public class SqlWriteJob {
 			
 			Object data = field.getValue();
 			
-			if ( (field.getKey() == keyName) && endKey ) {
+			if ( (field.getKey() == keyName) && writeKeyInWhereClause ) {
 				System.out.println("Detected an endKey.");
 				continue; // if we want the key last
 			}
@@ -154,39 +185,47 @@ public class SqlWriteJob {
 			i++;
 			
 			ps = insertData( ps, type, data, i) ;
-			
 		}
 		
-		if ( endKey ) {
+		if ( writeKeyInWhereClause ) {
 			ps = insertData( ps, keyName.charAt(0), row.get(keyName), total);
 		}
 		
 		return ps;
 	}
 
-
-	private PreparedStatement insertData(PreparedStatement ps, char type, Object data, int i) throws SQLException {
-		
+	/**
+	 * Put values into the ? of sql statements
+	 * 
+	 * @param ps
+	 * @param type
+	 * @param data
+	 * @param i
+	 * 
+	 * @return a ready-to-execute prepared statement
+	 * 
+	 * @throws SQLException
+	 * @throws DMSException
+	 */
+	private static PreparedStatement insertData(PreparedStatement ps, char type, Object data, int i) throws SQLException, DMSException {
 		switch (type) {
 			case STRING:
-				//System.out.println(field.getKey()+"-->String " +(String)data);
 				ps.setString(i, (String)data);
 				break;
 			case INTEGER:
-				//System.out.println(field.getKey()+"-->Integer " +(Integer)data);
 				ps.setInt(i, (Integer)data);
 				break;
 			case BOOLEAN:
-				//System.out.println(field.getKey()+"-->Boolean " +(Boolean)data);
 				ps.setBoolean(i, (Boolean)data);
 				break;
 			case DATE:
-				//System.out.println(field.getKey()+"-->Date " +Date.valueOf( (String)data));
 				ps.setDate(i, Date.valueOf( (String)data) );
 				break;
-			default:
-				ps.setNull(i, java.sql.Types.NULL);
+			case AUTO_INCREMENT:
+				ps.setNull(i, java.sql.Types.NULL );
 				break;
+			default:
+				throw new DMSException("Unknown sql type \""+type+"\".");
 		}
 		return ps;
 	}
